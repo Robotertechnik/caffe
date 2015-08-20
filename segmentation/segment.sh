3a4925f49cbb48f4d7b60d42563d1dd4066761dd
ctrevino <trevino.carlos.89@gmail.com>
2015-08-20 16:31:12 +0200

    grayscale segmentation scripts

--- segmentation/segment.sh
+++ segmentation/segment.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
-ffmpeg -i Google\ Drive/A\&R\ TU\ Dortmund/4th\ Semester\ 2015/Master\ thesis/Computer\ Vision\ Thesis/CamVid\ Database/Videos/0005VD.MXF -s 720x960 image%05d.png
-./caffe-master/segmentation/print_segmentation.py
+mkdir raw_images
+mkdir segmented_images
+ffmpeg -i Google\ Drive/A\&R\ TU\ Dortmund/4th\ Semester\ 2015/Master\ thesis/Computer\ Vision\ Thesis/CamVid\ Database/Videos/0005VD.MXF -s 960x720 raw_images/image%05d.png
+./caffe-master/segmentation/semantic_segmentation.py raw_images  \
+caffe-master/models/fcn_bvlc_alexnet/Alexnet_32s/deploy.prototxt  \
+caffe-master/models/fcn_bvlc_alexnet/Alexnet_32s/snapshot/train_iter_500.caffemodel  \
+segmented_images
+
